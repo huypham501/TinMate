829d71c106db76d926df9db1a953afc099250525
phamgiahuy0501 <52104180+phamgiahuy0501@users.noreply.github.com>
2021-04-05 00:49:18 +0700

    Update AddDemandActivity and layout

--- app/src/main/java/com/hcmus/tinuni/Activity/AddDemandActivity.java
+++ app/src/main/java/com/hcmus/tinuni/Activity/AddDemandActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,8 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hcmus.tinuni.Model.Demand;
 import com.hcmus.tinuni.R;
 
@@ -24,6 +28,7 @@ public class AddDemandActivity extends Activity {
     private EditText editTextSubject, editTextMajor, editTextSchool, editTextLevel;
     private Button buttonSave, buttonBack;
     private ProgressBar progressBar;
+    private TextView textViewDuplicateDemandWarning;
 
     private String strEditTextSubject;
     private String strEditTextMajor;
@@ -43,6 +48,8 @@ public class AddDemandActivity extends Activity {
         editTextMajor = findViewById(R.id.editTextMajor);
         editTextSchool = findViewById(R.id.editTextSchool);
 
+        textViewDuplicateDemandWarning = findViewById(R.id.textViewDuplicateDemandWarning);
+
         progressBar = findViewById(R.id.progressBar);
 
         buttonSave = findViewById(R.id.buttonSave);
@@ -50,23 +57,41 @@ public class AddDemandActivity extends Activity {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
+                textViewDuplicateDemandWarning.setVisibility(View.INVISIBLE);
 
                 initIdUser();
 
                 if (isValidForm()) {
                     Demand demand = new Demand(strEditTextSubject, strEditTextMajor, strEditTextSchool);
-                    databaseReference.push().setValue(demand).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    databaseReference.addValueEventListener(new ValueEventListener() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(AddDemandActivity.this, "Error commit data", Toast.LENGTH_SHORT).show();
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.exists()) {
                                 progressBar.setVisibility(View.INVISIBLE);
+                                textViewDuplicateDemandWarning.setVisibility(View.VISIBLE);
                             } else {
-                                progressBar.setVisibility(View.INVISIBLE);
-                                AddDemandActivity.super.onBackPressed();
+                                databaseReference.push().setValue(demand).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (!task.isSuccessful()) {
+                                            Toast.makeText(AddDemandActivity.this, "Error commit data", Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                        } else {
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                            AddDemandActivity.super.onBackPressed();
+                                        }
+                                    }
+                                });
                             }
                         }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
                     });
+
                 } else {
                     progressBar.setVisibility(View.INVISIBLE);
                 }
