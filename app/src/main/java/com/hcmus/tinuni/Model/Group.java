e0ac858d45a43c7ed5d421c6d691931e3e776858
phamgiahuy0501 <52104180+phamgiahuy0501@users.noreply.github.com>
2021-04-17 05:43:46 +0700

    Update MatchGroup, Group, LoadingActivity

--- app/src/main/java/com/hcmus/tinuni/Model/Group.java
+++ app/src/main/java/com/hcmus/tinuni/Model/Group.java
@@ -2,16 +2,27 @@ package com.hcmus.tinuni.Model;
 
 public class Group {
     String id;
+
     String name;
     String imageURL;
+    String subject;
+    String major;
+    String school;
+    public Group(String id, String name, String imageURL, String subject, String major, String school) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
+        this.subject = subject;
+        this.major = major;
+        this.school = school;
+    }
 
-    // Participants
+// Participants
     // Demand
 
     public Group() {
 
     }
-
     public Group(String id, String name, String imageURL) {
         this.id = id;
         this.name = name;
@@ -40,5 +51,41 @@ public class Group {
 
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", subject='" + subject + '\'' +
+                ", major='" + major + '\'' +
+                ", school='" + school + '\'' +
+                '}';
     }
 }
