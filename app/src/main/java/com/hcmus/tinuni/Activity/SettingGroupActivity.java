4a49a0ea328d4d20fbc7f89feb5b408b046538a0
ntan1902 <trinh.an.1902@gmail.com>
2021-04-19 21:13:17 +0700

    Update SettingGroup

--- app/src/main/java/com/hcmus/tinuni/Activity/SettingGroupActivity.java
+++ app/src/main/java/com/hcmus/tinuni/Activity/SettingGroupActivity.java
@@ -10,17 +10,25 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hcmus.tinuni.Activity.Profile.ShowProfileActitivy;
+import com.hcmus.tinuni.Model.Group;
 import com.hcmus.tinuni.R;
 
 public class SettingGroupActivity extends Activity {
     private ImageView ivAvatar, btnAdd, btnGoBack;
     private TextView txtName;
 
-    private RelativeLayout layoutMembers, layoutChangeName, layoutPrivate;
+    private RelativeLayout layoutMembers, layoutChangeName, layoutLeave, layoutChangeAvatar;
 
     private String groupId;
 
@@ -36,15 +44,35 @@ public class SettingGroupActivity extends Activity {
         btnAdd = findViewById(R.id.btnAdd);
         btnGoBack = findViewById(R.id.btnGoBack);
         txtName = findViewById(R.id.name);
+
         layoutMembers = findViewById(R.id.layoutMembers);
         layoutChangeName = findViewById(R.id.layoutChangeName);
-        layoutPrivate = findViewById(R.id.layoutPrivate);
+        layoutLeave = findViewById(R.id.layoutLeave);
+        layoutChangeAvatar = findViewById(R.id.layoutChangeAvatar);
 
         Intent i = getIntent();
         groupId = i.getStringExtra("groupId");
         Log.e("SettingActivity: ", groupId);
         mUser = FirebaseAuth.getInstance().getCurrentUser();
-        mRef = FirebaseDatabase.getInstance().getReference("Groups");
+
+        mRef = FirebaseDatabase.getInstance().getReference("Groups").child(groupId);
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Group group = snapshot.getValue(Group.class);
+
+                txtName.setText(group.getName());
+                Glide.with(SettingGroupActivity.this)
+                        .load(group.getImageURL())
+                        .into(ivAvatar);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +87,34 @@ public class SettingGroupActivity extends Activity {
             @Override
             public void onClick(View v) {
                 SettingGroupActivity.this.onBackPressed();
+            }
+        });
+
+        layoutMembers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        layoutChangeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        layoutChangeAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        layoutLeave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
