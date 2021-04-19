8685e1b93ef3a753d2ba49b61d6faf99916ccd82
ntan1902 <trinh.an.1902@gmail.com>
2021-04-19 16:26:10 +0700

    Fix UI Conversation fragment

--- app/src/main/java/com/hcmus/tinuni/Fragment/ConversationFragment.java
+++ app/src/main/java/com/hcmus/tinuni/Fragment/ConversationFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.hcmus.tinuni.Adapter.ViewPageAdapter;
@@ -18,7 +20,7 @@ public class ConversationFragment extends Fragment {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPageAdapter adapter;
-    private View indicator;
+    private TextView indicator;
     private int indicatorWidth;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,23 +35,28 @@ public class ConversationFragment extends Fragment {
         tabLayout = view.findViewById(R.id.tabLayout);
         viewPager = view.findViewById(R.id.viewPager);
         indicator = view.findViewById(R.id.indicator);
+
         adapter = new ViewPageAdapter(getChildFragmentManager());
         adapter.addFragment(new ChatFragment(),"Chats");
         adapter.addFragment(new GroupFragment(),"Groups");
+
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
         tabLayout.post(new Runnable() {
             @Override
             public void run() {
-                indicatorWidth = tabLayout.getWidth() / tabLayout.getTabCount();
+                indicatorWidth = tabLayout.getWidth() / tabLayout.getTabCount() - 25;
+
 
                 //Assign new width
-                FrameLayout.LayoutParams indicatorParams = (FrameLayout.LayoutParams) indicator.getLayoutParams();
+                RelativeLayout.LayoutParams indicatorParams = (RelativeLayout.LayoutParams) indicator.getLayoutParams();
                 indicatorParams.width = indicatorWidth;
                 indicator.setLayoutParams(indicatorParams);
+
             }
         });
+
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
@@ -58,7 +65,7 @@ public class ConversationFragment extends Fragment {
             //see https://developer.android.com/reference/android/support/v4/view/ViewPager.OnPageChangeListener
             @Override
             public void onPageScrolled(int i, float positionOffset, int positionOffsetPx) {
-                FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) indicator.getLayoutParams();
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) indicator.getLayoutParams();
 
                 //Multiply positionOffset with indicatorWidth to get translation
                 float translationOffset =  (positionOffset+i) * indicatorWidth ;
@@ -73,6 +80,25 @@ public class ConversationFragment extends Fragment {
 
             @Override
             public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                String title = tab.getText().toString();
+                indicator.setText(title);
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
