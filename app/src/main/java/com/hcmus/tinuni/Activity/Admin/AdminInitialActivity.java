da1fc05fbb28e9f77f3537ceb661b640e50f1507
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-04-20 00:21:15 +0700

    Manage demand -> Admin diary
    -  Rename some files & change icon in AdminHomeFragment

--- app/src/main/java/com/hcmus/tinuni/Activity/Admin/AdminInitialActivity.java
+++ app/src/main/java/com/hcmus/tinuni/Activity/Admin/AdminInitialActivity.java
@@ -1,40 +1,27 @@
 package com.hcmus.tinuni.Activity.Admin;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.hcmus.tinuni.Activity.Authentication.SignInActivity;
-import com.hcmus.tinuni.Activity.Profile.UserProfileActitivy;
-import com.hcmus.tinuni.Fragment.Admin.AdminDemandFragment;
+import com.hcmus.tinuni.Fragment.Admin.AdminDiaryFragment;
 import com.hcmus.tinuni.Fragment.Admin.AdminHomeFragment;
 import com.hcmus.tinuni.Fragment.Admin.AdminRoomFragment;
 import com.hcmus.tinuni.Fragment.Admin.AdminUserFragment;
 import com.hcmus.tinuni.R;
-
-import static android.widget.Toast.LENGTH_SHORT;
 
 public class AdminInitialActivity extends FragmentActivity implements AdminHomeFragment.AdminHomeFragmentListener{
     //---------------------------------------------
@@ -47,7 +34,7 @@ public class AdminInitialActivity extends FragmentActivity implements AdminHomeF
     private AdminHomeFragment homeFrag;
     private AdminRoomFragment roomFrag;
     private  AdminUserFragment userFrag;
-    private  AdminDemandFragment demandFrag;
+    private AdminDiaryFragment demandFrag;
     //---------------------------------------------
     protected BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -69,7 +56,7 @@ public class AdminInitialActivity extends FragmentActivity implements AdminHomeF
                             toolbar_title.setText("MANAGE USER");
                             break;
                         case R.id.manageDemandFragment:
-                            selectedFragment = new AdminDemandFragment();
+                            selectedFragment = new AdminDiaryFragment();
                             toolbar_title.setText("MANAGE DEMAND");
                             break;
                     }
@@ -122,7 +109,7 @@ public class AdminInitialActivity extends FragmentActivity implements AdminHomeF
         homeFrag = new AdminHomeFragment();
         roomFrag = new AdminRoomFragment();
         userFrag = new AdminUserFragment();
-        demandFrag = new AdminDemandFragment();
+        demandFrag = new AdminDiaryFragment();
 
         //Stick AdminHomeFragment to screen
         getSupportFragmentManager().beginTransaction().replace(R.id.admin_fragment_container, homeFrag).commit();
--- app/src/main/java/com/hcmus/tinuni/Fragment/Admin/AdminDiaryFragment.java
+++ app/src/main/java/com/hcmus/tinuni/Fragment/Admin/AdminDiaryFragment.java
@@ -1,26 +1,20 @@
 package com.hcmus.tinuni.Fragment.Admin;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.hcmus.tinuni.R;
 
-public class AdminDemandFragment extends Fragment {
+public class AdminDiaryFragment extends Fragment {
     //-----------------------------------------------------
 
     //-----------------------------------------------------
-    public AdminDemandFragment() {
+    public AdminDiaryFragment() {
         // Required empty public constructor
     }
 
@@ -28,7 +22,7 @@ public class AdminDemandFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_admin_demand, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_diary, container, false);
 
         //Animated background
 //        RelativeLayout relativeLayout = view.findViewById(R.id.animated_gradient_bg);
