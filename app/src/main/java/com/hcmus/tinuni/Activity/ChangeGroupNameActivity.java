a9e9c607197efcea4328e1d3d194bbb05cb123c9
ohnguyen2404 <50840294+ohnguyen2404@users.noreply.github.com>
2021-05-07 18:00:56 +0700

    Finish all group settings function

---
+++ app/src/main/java/com/hcmus/tinuni/Activity/ChangeGroupNameActivity.java
@@ -0,0 +1,103 @@
+package com.hcmus.tinuni.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hcmus.tinuni.Model.Group;
+import com.hcmus.tinuni.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class ChangeGroupNameActivity extends Activity {
+    ImageView btnGoBack, ivAvatar;
+    EditText edtGroupName;
+    TextView btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_group_name);
+
+        ivAvatar = findViewById(R.id.ivAvatar);
+        edtGroupName = findViewById(R.id.edtGroupName);
+        btnSave = findViewById(R.id.btnSave);
+        btnGoBack = findViewById(R.id.btnGoBack);
+
+        Intent i = getIntent();
+        String groupId = i.getStringExtra("groupId");
+        String groupName = i.getStringExtra("groupName");
+        String img_link = i.getStringExtra("imageURL");
+
+        System.out.println("name: " + groupName);
+        System.out.println("url: " + img_link);
+        edtGroupName.setText(groupName);
+        Glide.with(ChangeGroupNameActivity.this)
+                .load(img_link)
+                .into(ivAvatar);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String new_groupName = edtGroupName.getText().toString();
+                if (!new_groupName.equals("")) {
+                    // 5. Confirm success
+                    new SweetAlertDialog(ChangeGroupNameActivity.this, SweetAlertDialog.WARNING_TYPE)
+                            .setTitleText("Are you sure to save?")
+                            .setConfirmText("Yes")
+                            .setCancelButton("Cancel", new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+                                    sDialog.dismissWithAnimation();
+                                }
+                            })
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+
+                                    DatabaseReference groupRef = FirebaseDatabase.getInstance().getReference("Groups").child(groupId).child("name");
+                                    groupRef.setValue(new_groupName);
+
+                                    sDialog
+                                            .setTitleText("Saved!")
+                                            .setContentText("Change group name successfully")
+                                            .setConfirmText("OK")
+                                            .setConfirmClickListener(null)
+                                            .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                                    ChangeGroupNameActivity.this.onBackPressed();
+                                }
+                            })
+                            .show();
+                } else {
+                    Toast.makeText(ChangeGroupNameActivity.this, "Please fill in the name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeGroupNameActivity.this.onBackPressed();
+            }
+        });
+    }
+}
--- app/src/main/java/com/hcmus/tinuni/Activity/SettingGroupActivity.java
+++ app/src/main/java/com/hcmus/tinuni/Activity/SettingGroupActivity.java
@@ -2,17 +2,25 @@ package com.hcmus.tinuni.Activity;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,9 +28,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.hcmus.tinuni.Activity.Profile.EditProfileActivity;
 import com.hcmus.tinuni.Activity.Profile.ShowProfileActitivy;
 import com.hcmus.tinuni.Model.Group;
 import com.hcmus.tinuni.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class SettingGroupActivity extends Activity {
     private ImageView ivAvatar, btnAdd, btnGoBack;
@@ -30,15 +45,23 @@ public class SettingGroupActivity extends Activity {
 
     private RelativeLayout layoutMembers, layoutChangeName, layoutLeave, layoutChangeAvatar;
 
-    private String groupId;
+    private String groupId, img_link;
 
     private FirebaseUser mUser;
     private DatabaseReference mRef;
+
+    private Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_group);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         ivAvatar = findViewById(R.id.ivAvatar);
         btnAdd = findViewById(R.id.btnAdd);
@@ -49,6 +72,12 @@ public class SettingGroupActivity extends Activity {
         layoutChangeName = findViewById(R.id.layoutChangeName);
         layoutLeave = findViewById(R.id.layoutLeave);
         layoutChangeAvatar = findViewById(R.id.layoutChangeAvatar);
+
+        //Init AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingGroupActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.layout_loading_dialog);
+        alertDialog = builder.create();
 
         Intent i = getIntent();
         groupId = i.getStringExtra("groupId");
@@ -61,6 +90,7 @@ public class SettingGroupActivity extends Activity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Group group = snapshot.getValue(Group.class);
 
+                img_link = group.getImageURL();
                 txtName.setText(group.getName());
                 Glide.with(SettingGroupActivity.this)
                         .load(group.getImageURL())
@@ -93,29 +123,136 @@ public class SettingGroupActivity extends Activity {
         layoutMembers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent viewMembers = new Intent(SettingGroupActivity.this, ViewMembersGroupActivity.class);
+                viewMembers.putExtra("groupId", groupId);
+                startActivity(viewMembers);
             }
         });
 
         layoutChangeName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent changeName = new Intent(SettingGroupActivity.this, ChangeGroupNameActivity.class);
+                changeName.putExtra("groupId", groupId);
+                changeName.putExtra("groupName", txtName.getText());
+                changeName.putExtra("imageURL", img_link);
+                startActivity(changeName);
             }
         });
 
         layoutChangeAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                choosePicture();
             }
         });
 
         layoutLeave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Sweet Alert start
+                new SweetAlertDialog(SettingGroupActivity.this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure to leave the group?")
+                        .setConfirmText("Yes")
+                        .setCancelButton("Cancel", new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                            }
+                        })
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                DatabaseReference groupRef = FirebaseDatabase.getInstance().getReference("Groups").child(groupId).child("Participants").child(mUser.getUid());
+                                groupRef.removeValue();
 
+                                sDialog
+                                        .setTitleText("Group left !")
+                                        .setContentText("Group left !")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(null)
+                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+
+                                Intent i = new Intent(SettingGroupActivity.this, MainActivity.class);
+                                startActivity(i);
+                            }
+                        })
+                        .show();
+                //Sweet Alert end
             }
         });
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            ivAvatar.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        StorageReference storageRef = storageReference.child("images/groups/" + System.currentTimeMillis() + "." + getFileExtension(imageUri));
+        storageRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                String old_img = img_link;
+                //add new image
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        img_link = uri.toString();
+                        DatabaseReference groupRef = FirebaseDatabase.getInstance().getReference("Groups").child(groupId).child("imageURL");
+                        groupRef.setValue(img_link);
+                        //delete old image
+                        if (!old_img.matches("default") && old_img.contains("firebasestorage")) {
+                            StorageReference deleteRef = storage.getReferenceFromUrl(old_img);
+                            deleteRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    System.out.println("Delete old image successfully !");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    System.out.println("Delete old image failed !");
+                                }
+                            });
+                        }
+                        alertDialog.dismiss();
+                        Toast.makeText(SettingGroupActivity.this, "Upload SUCCESS", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                alertDialog.show();
+//                Toast.makeText(SettingGroupActivity.this, "Uploadinggg", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                alertDialog.dismiss();
+                Toast.makeText(SettingGroupActivity.this, "Upload failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private String getFileExtension(Uri mUri) {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(mUri));
     }
 }
